d1465f7c7b6673664c80d386e44149d216177e18
jjmartel <juanomartel@gmail.com>
2025-05-28 19:02:26 +0000

    avanzando

--- src/front/components/Signup.jsx
+++ src/front/components/Signup.jsx
@@ -6,36 +6,32 @@ export const Signup = () => {
       <div className="d-flex shadow rounded overflow-hidden signup-container">
         
        
-        <div className="signup-image d-none d-md-block text-muted  ">
+        <div className="signup-image d-none d-md-block text-muted">
             <p className="fw-bold position-absolute"> Step 1: Basic Info </p>
-          <img
-            src="https://picsum.photos/200/300" // reemplazá por tu imagen real
-            alt="photo-work"
-            className="img-fluid w-100 h-100 object-fit-cover"
-          />
+          <img src="https://picsum.photos/200/300" alt="photo-work" className="img-fluid w-100 h-100 object-fit-cover opacity-50"/>
         </div>
 
-        {/*Formulario de registro del usuario */}
-        <div className="signup-form bg-primary text-white p-4">
+        
+        <div className="signup-form bg-primary text-dark p-4">
           <h4 className="fw-bold">Let’s Get Started</h4>
           <p className="mb-4">Join our community</p>
 
           <form>
-            <div className="mb-5">
-              <label htmlFor="email" className="form-label">Email:</label>
-              <input type="email" className="form-control rounded-pill" id="email" />
+            <div className="mb-5 d-flex align-items-center">
+              <label htmlFor="email" className="form-label fw-bold ">Email:</label>
+              <input type="email" className="form-control rounded-pill "  id="email" />
             </div>
 
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">
+            <div className="mb-5  d-flex align-items-center">
+              <label htmlFor="password" className="form-label fw-bold">
                 Password: <small>(min. 8 characters)</small>
               </label>
               <input type="password" className="form-control rounded-pill" id="password" />
             </div>
 
-            <div className="mb-3 d-flex justify-content-between">
+            <div className="mb-5 d-flex justify-content-between">
               <div className="form-check">
-                <input className="form-check-input" type="radio" name="role" id="pro" />
+                <input className="form-check-input mb-4" type="radio" name="role" id="pro" />
                 <label className="form-check-label" htmlFor="pro">I'm a professional</label>
               </div>
               <div className="form-check">
