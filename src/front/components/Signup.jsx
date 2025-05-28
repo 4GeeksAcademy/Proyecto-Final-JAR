470d287174ee63a4b8e1373e6a4464f930120bea
jjmartel <juanomartel@gmail.com>
2025-05-28 17:14:54 +0000

    basicSignup

---
+++ src/front/components/Signup.jsx
@@ -0,0 +1,18 @@
+import { Navbar } from "./Navbar"
+
+export const Signup = () => {   
+    return (
+        <div> 
+            <Navbar/> 
+            Aca va el coso 
+
+        </div>
+
+
+    )
+
+
+
+
+
+}
