501e9538879b05c47fcba83ab587eaa5dbd43ef7
Jose Andrés Lázaro <59710168+Lazaroth93@users.noreply.github.com>
2025-06-03 16:02:29 +0000

    Comienzo de About a falta de cambios

---
+++ src/front/components/About.jsx
@@ -0,0 +1,55 @@
+import { Navbar } from "./Navbar"
+
+import "../about.css";
+
+
+export const About = () => {
+    return (
+        <div className="container about-container">
+
+            <img src="https://via.assets.so/img.jpg?w=400&h=150&tc=blue&bg=#cecece" alt="" />
+
+
+
+            <p className="about-text">Star Gigs: The perfect bridge between clients and businesses</p>
+
+            <p className="about-text">
+                In a world where connection and efficiency are key, Star Gigs stands as the ideal link between clients and businesses, ensuring smooth communication, real opportunities, and effective results.
+            </p>
+
+            <p className="about-text">
+                Our platform makes it easier for professionals from various industries to connect with clients seeking reliable solutions, providing a space where experience and innovation come together to drive successful projects.
+            </p>
+
+            <p className="about-text">
+                Whether it's finding the perfect talent or accessing strategic business opportunities, Star Gigs transforms the way businesses and clients collaborate, creating strong and efficient relationships in a modern and dynamic environment.
+            </p>
+
+
+
+            <div className="  container form-container m-5">
+                <h2 className="form-tittle">Contact Form </h2>
+                <form className=" row from-text">
+                    <div class=" col-6 mb-3">
+                        <label for="exampleInputEmail1" className="form-label ">Email </label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    
+                    </div>
+                    <div className=" col-6 mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Name</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                  
+
+                    <div class="mb-3 col-12 message-form">
+                        <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div className="col-2">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,6 +10,7 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { SignupPage } from "./pages/Signup"; // 🔹 Usa el nombre correcto
+import { AboutPage } from "./pages/About"; 
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -27,6 +28,7 @@ export const router = createBrowserRouter(
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Route>
     )
 );
