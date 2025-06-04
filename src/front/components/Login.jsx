a62ae7262f36991a3092d16328f34c9c33514c0b
jjmartel <juanomartel@gmail.com>
2025-06-04 18:09:32 +0000

    loginFinish

--- src/front/components/Login.jsx
+++ src/front/components/Login.jsx
@@ -1,24 +1,24 @@
 import { Navbar } from "./Navbar"
-import "../login.css"
+import "../login.css";
+
 
 export const Login = () => {
 
-    
+
     const handleSubmit = e => {
         e.preventDefault()
     }
 
     return (
-        <div className="login-container-wrapper"> 
+        <div className="login-container-wrapper">
             <div className="login-main-container">
-                <div className="login-image-section">
-                    <p className="login-step">Step 1: Basic Info</p>
-                    <img src="https://picsum.photos/200/300" alt="photo-work" className="login-image" />
+                <div className="login-image-section ">
+                    <img src="https://picsum.photos/200/300" alt="photo-work" className="signup-image" />
                 </div>
 
                 <div className="login-form-section">
-                    <h4 className="login-title">Let's Get Started</h4>
-                    <p className="login-title2">Join our community</p>
+                    <h4 className="login-title">Great to see you again!</h4>
+                    <p className="login-title2">Let's get back to work</p>
 
                     <form onSubmit={handleSubmit}>
                         <div className="form-field-group">
@@ -27,13 +27,23 @@ export const Login = () => {
                         </div>
 
                         <div className="form-field-group">
-                            <label htmlFor="password" className="form-label">Password: <span className="password-hint">(min. 8 characters)</span></label>
-                            <input type="password" className="form-input" id="password"/>
+                            <label htmlFor="password" className="form-label">Password:</label>
+                            <input type="password" className="form-input" id="password" />
                         </div>
 
 
                         <div className="submit-button-container">
                             <button type="submit" className="submit-button">Continue</button>
+                        </div>
+
+                        <div className="form-options">
+                            <div className="remember-me">
+                                <input type="checkbox" id="remember" />
+                                <label htmlFor="remember"> Remember me</label>
+                            </div>
+                            <div className="forgot-password">
+                                <a href="#">Forgot password?</a>
+                            </div>
                         </div>
                     </form>
                 </div>
