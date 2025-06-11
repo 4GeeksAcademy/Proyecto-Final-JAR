37e51dc1bcaf759328db52c311921ba44ab43e37
Jose Andrés Lázaro <59710168+Lazaroth93@users.noreply.github.com>
2025-06-11 18:20:33 +0000

    comienzo view profile

---
+++ src/front/components/Profile.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./Navbar"
+
+import "../../front/pricing.css"
+
+export const Pricing = () => {
+    return (
+        <div className="container-fluid pricingCustom  text-center p-5 ">
+            <h1 className="py-3 pageTittle">Profile </h1>
+
+           
+        </div>
+    );
+};
